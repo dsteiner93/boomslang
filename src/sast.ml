88f1a5f81f1eded87fb3a8f811ba24fc644eef54
Robert Kim <robert23kim@gmail.com>
2021-03-22 19:06:55 -0400

    wip

---
+++ src/sast.ml
@@ -0,0 +1,21 @@
+(* Semantically-checked Abstract Syntax Tree and functions for printing it *)
+
+open Ast
+
+type sexpr = typ * sx
+and sx = 
+  SStringLiteral of string
+| Scall of scall
+and scall = 
+  SFuncCall of string * sexpr list (* my_func(1, 2, 3) *)
+
+type sstmt =
+  SExpr of sexpr
+
+type sp_unit =
+  SStmt of sstmt
+
+type sprogram = sp_unit list
+(* or maybe just type sprogram = sexpr *)
+
+(* [ (Void, ("println", [(string, "Hello World")]))] *)
