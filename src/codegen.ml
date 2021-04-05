c0c063e19d263b7e24d2290e79331a4a84d98d0c
Nathan Cuevas <nathanjcuevas@gmail.com>
2021-04-04 19:43:28 -0700

    added support for println and int_to_string

--- src/codegen.ml
+++ src/codegen.ml
@@ -32,7 +32,7 @@ let translate sp_units =
   let i8_t       = L.i8_type            context
   and i32_t      = L.i32_type           context
   and void_t     = L.void_type          context
-  and str_t      = L.pointer_type (L.i8_type context)
+  and str_t      = L.pointer_type       (L.i8_type context)
   in
 
   (* Return the LLVM type for a Boomslang type *)
@@ -45,49 +45,55 @@ let translate sp_units =
 
   (* create a map of all of the built in functions *)
   let built_in_map =
-   let built_in_funcs : (string * A.typ * (A.typ list)) list = [
-     ("println", A.Primitive(Void), [A.Primitive(String)])
+   let built_in_funcs : (string * A.typ * (A.typ list)) list = [ (*put funcs here *)
+     ("println", A.Primitive(Void), [A.Primitive(String)]);
+     ("int_to_string", A.Primitive(String), [A.Primitive(Int)])
    ] in
    let helper m e = match e with fun_name, ret_t, arg_ts -> 
-       let arg_t_arr = Array.of_list (List.fold_left (fun s e -> s @ [ltype_of_typ e]) 
-                    [] arg_ts) in
-    SignatureMap.add { S.fs_name = fun_name; S.formal_types = arg_ts }
-                      (L.var_arg_function_type (ltype_of_typ ret_t) arg_t_arr) m in
+    let arg_t_arr = Array.of_list 
+                    (List.fold_left (fun s e -> s @ [ltype_of_typ e]) [] arg_ts) in
+    let func = L.declare_function fun_name 
+              (L.var_arg_function_type (ltype_of_typ ret_t) arg_t_arr) the_module in
+    let signature = { S.fs_name = fun_name; S.formal_types = arg_ts } in
+    SignatureMap.add signature func m in
    List.fold_left helper SignatureMap.empty built_in_funcs in
-    
-  (* define the built-in println function *)
-  let println_t : L.lltype = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
-  let println_func : L.llvalue = L.declare_function "printf" println_t the_module in
 
+  (* expression builder *)
+  let rec build_expr builder (exp : sexpr) = match exp with
+    _, SIntLiteral(i)      -> L.const_int i32_t i
+  | _, SStringLiteral(str) -> L.build_global_stringptr str "unused" builder
+  | typ, SCall(sc) -> match sc with
+      SFuncCall(func_name, expr_list) -> 
+       let expr_typs = List.fold_left (fun s (t, _) -> s @ [t]) [] expr_list in
+       let signature = { S.fs_name = func_name; S.formal_types = expr_typs } in
+       if SignatureMap.mem signature built_in_map
+        then L.build_call (SignatureMap.find signature built_in_map)
+             (Array.of_list (List.fold_left (fun s e -> s @ [build_expr builder e]) 
+             [] expr_list))
+             (if typ = Primitive(Void) then "" else (func_name ^ "_res"))
+             builder
+       else L.const_int i32_t 0 (* TODO: this is a placeholder! *)
+  in
+
+  (* statement builder *) 
+  let build_stmt  builder (ss : sstmt) = match ss with
+    SExpr(se)   -> ignore (build_expr builder se)
+  | _           -> () in
+
+  (* function decleration builder *) 
+  let build_func  (sf : sfdecl) = () in 
+
+  (* class decleration builder *) 
+  let build_class (sc : sclassdecl) = () in
+  
   (* LLVM requires a 'main' function as an entry point *)
   let main_t : L.lltype =
       L.var_arg_function_type i32_t [| |] in
   let main_func : L.llvalue =
     L.define_function "main" main_t the_module in
-
-  let str_format_str builder =
-   L.build_global_stringptr "%s\n" "fmt" builder in
-
-  let rec build_expr builder (exp : sexpr) = match exp with
-    _, SIntLiteral(i)      -> L.const_int i32_t i
-  | _, SStringLiteral(str) -> L.build_global_stringptr str "unused" builder
-  | typ, SCall(sc) -> match sc with
-      SFuncCall(func_name, expr_list) -> match func_name, expr_list with
-        "println", [e] -> L.build_call println_func [| (str_format_str builder); 
-                          (build_expr builder e) |] "" builder
-  in
-
-  let build_stmt  builder (ss : sstmt) = match ss with
-    SExpr(se)   -> ignore (build_expr builder se)
-  | _           -> () in
-
-  let build_func  (sf : sfdecl) = () in 
-
-  let build_class (sc : sclassdecl) = () in
-  
-  (* define the builder for the main block *) 
   let main_builder = L.builder_at_end context (L.entry_block main_func) in
 
+  (* program builder *) 
   let build_program (spunit : sp_unit) = match spunit with
     SStmt(ss)       -> build_stmt main_builder ss
   | SFdecl(sf)      -> build_func sf
